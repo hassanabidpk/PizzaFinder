13bdaf09bd1638cf2a93a36f942fc905b0133b47
Hassan Abid <hassanabidpk89@gmail.com>
2019-11-13 20:37:38 +0800

    add basic jetpack compose activity

---
+++ app/src/main/java/dev/hassanabid/pizzafinder/MainActivity.kt
@@ -0,0 +1,235 @@
+package dev.hassanabid.pizzafinder
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.os.Bundle
+import android.os.Handler
+import android.os.ResultReceiver
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.compose.View
+import androidx.core.app.ActivityCompat
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dev.hassanabid.pizzafinder.utils.Constants
+import dev.hassanabid.pizzafinder.utils.FetchAddressIntentService
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
+    private val ADDRESS_REQUESTED_KEY = "address-request-pending"
+    private val LOCATION_ADDRESS_KEY = "location-address"
+
+
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var lastLocation: Location? = null
+    private var addressRequested = false
+    private var addressOutput = ""
+    private lateinit var resultReceiver: AddressResultReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Greeting("Android")
+            }
+        }
+
+        Log.d("PizzaApp", "onCreate")
+        resultReceiver = AddressResultReceiver(Handler())
+
+        // Set defaults, then update using values stored in the Bundle.
+        addressRequested = true
+        addressOutput = ""
+        updateValuesFromBundle(savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (!checkPermissions()) {
+            requestPermissions()
+        } else {
+            getAddress()
+        }
+    }
+
+    /**
+     * Updates fields based on data stored in the bundle.
+     */
+    private fun updateValuesFromBundle(savedInstanceState: Bundle?) {
+        savedInstanceState ?: return
+
+        ADDRESS_REQUESTED_KEY.let {
+            // Check savedInstanceState to see if the address was previously requested.
+            if (savedInstanceState.keySet().contains(it)) {
+                addressRequested = savedInstanceState.getBoolean(it)
+            }
+        }
+
+        LOCATION_ADDRESS_KEY.let {
+            // Check savedInstanceState to see if the location address string was previously found
+            // and stored in the Bundle. If it was found, display the address string in the UI.
+            if (savedInstanceState.keySet().contains(it)) {
+                addressOutput = savedInstanceState.getString(it)
+                displayAddressOutput()
+            }
+        }
+
+
+    }
+
+    fun displayAddressOutput(){
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getAddress() {
+        fusedLocationClient?.lastLocation?.addOnSuccessListener { location ->
+            if (location == null) {
+                Log.w(TAG, "onSuccess:null")
+                return@addOnSuccessListener
+            }
+
+            lastLocation = location
+
+            // Determine whether a Geocoder is available.
+            if (!Geocoder.isPresent()) {
+                Log.e(TAG, "no geocoder servuce available")
+                return@addOnSuccessListener
+            }
+
+            // If the user pressed the fetch address button before we had the location,
+            // this will be set to true indicating that we should kick off the intent
+            // service after fetching the location.
+            if (addressRequested) startIntentService()
+        }?.addOnFailureListener(this) { e -> Log.w(TAG, "getLastLocation:onFailure", e) }
+    }
+
+    private fun startIntentService() {
+
+        val intent = Intent(this, FetchAddressIntentService::class.java).apply {
+            putExtra(Constants.RECEIVER, resultReceiver)
+            putExtra(Constants.LOCATION_DATA_EXTRA, lastLocation)
+        }
+        startService(intent)
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState ?: return
+
+        with(savedInstanceState) {
+            // Save whether the address has been requested.
+            putBoolean(ADDRESS_REQUESTED_KEY, addressRequested)
+
+            // Save the address string.
+            putString(LOCATION_ADDRESS_KEY, addressOutput)
+        }
+
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    internal inner class AddressResultReceiver(handler: Handler) : ResultReceiver(handler) {
+
+        override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+
+            // Display the address string
+            // or an error message sent from the intent service.
+            val addressOutput = resultData?.getString(Constants.RESULT_DATA_KEY) ?: ""
+            Log.d("PizzaApp", "success - address : $addressOutput")
+            displayAddressOutput()
+
+            // Show a toast message if an address was found.
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                Log.d("PizzaApp", "success - address was found")
+            }
+
+            addressRequested = false
+
+        }
+    }
+
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+
+            ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSIONS_REQUEST_CODE)
+
+        } else {
+            Log.i(TAG, "Requesting permission")
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        Log.i(TAG, "onRequestPermissionResult")
+
+        if (requestCode != REQUEST_PERMISSIONS_REQUEST_CODE) return
+
+        when {
+            grantResults.isEmpty() ->
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.")
+            grantResults[0] == PackageManager.PERMISSION_GRANTED -> // Permission granted.
+                getAddress()
+            else -> {
+                // Permission denied.
+            }
+
+        }
+    }
+
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        Greeting("Android")
+    }
+
+
+}
+
+
---
+++ app/src/main/java/dev/hassanabid/pizzafinder/utils/Constants.kt
@@ -0,0 +1,10 @@
+package dev.hassanabid.pizzafinder.utils
+
+object Constants {
+    const val SUCCESS_RESULT = 0
+    const val FAILURE_RESULT = 1
+    const val PACKAGE_NAME = "dev.hassanabid.pizzafinder"
+    const val RECEIVER = "$PACKAGE_NAME.RECEIVER"
+    const val RESULT_DATA_KEY = "${PACKAGE_NAME}.RESULT_DATA_KEY"
+    const val LOCATION_DATA_EXTRA = "${PACKAGE_NAME}.LOCATION_DATA_EXTRA"
+}
